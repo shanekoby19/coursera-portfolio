77c7ab333978277c5ffe7794bc872769bc4de0da
Shane Kobylecky <skobylecky1@gmail.com>
2023-05-04 09:41:46 -0700

    create the landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, I'm Pete!";
+const bio1 = "A frontend developer,";
+const bio2 = "specialised in ";
+const bioSpan = "react".toUpperCase();;
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,7 +16,10 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <Avatar src="https://i.pravatar.cc/150?img=7" size='2xl'/>
+    <h1 style={{ fontSize: "3rem" }}>{greeting}</h1>
+    <h2 style={{ fontSize: "1.5rem" }}>{bio1}</h2>
+    <h2 style={{ fontSize: "1.5rem" }}>{bio2}<span style={{ fontWeight: 700, color: "#18181b"}}>{bioSpan}</span></h2>
   </FullScreenSection>
 );
 
